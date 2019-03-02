f476cef9bd676e515d7be146fb7c155fc2f018a8
surjan-kshetri <kshe9881@stthomas.edu>
2019-03-02 01:44:10 +0000

    script commit

---
+++ kshe9881-Lab02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+apt-get install -y tree
+apt-get install -y git
